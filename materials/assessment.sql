2248897906a854a2ee8979d83a5aea662e39cb74
Nick Ulle <nick.ulle@gmail.com>
2022-04-13 16:31:45 -0700

    Add assessment.
    Co-authored-by: Nicholas Alonzo <nicholas@diversifyds.org>

---
+++ materials/assessment.sql
@@ -0,0 +1,54 @@
+-- Intro to SQL for Querying Databases
+-- GradPathways Microcredential Assessment
+--
+-- To earn a micro-badge for this workshop, answer each of the following
+-- questions with a SQL query that produces the table described.  Please do not
+-- delete the questions, and write each answer beneath the corresponding
+-- question.
+--
+-- When you're done, make sure this file includes your name and UC Davis email
+-- address, save this file, and submit it to the GradPathways portal.
+--
+-- For any questions, please contact DataLab or GradPathways, NOT Nicholas
+-- Alonzo.
+--
+-- Created by Nicholas Alonzo <nicholas@diversifyds.org>
+-- Maintained by UC Davis DataLab <https://datalab.ucdavis.edu/>
+
+
+-- 1. Get a table of all Woodland residents whose phone number has area code
+--    530.
+
+
+-- 2. Get a table of all books published in the 80s by "Cedar House Publishers"
+--    and "Sound & Seas Co."
+      
+
+-- 3. Get a table of the titles of all books where the genre is missing.
+
+
+-- 4. Get a table which contains the average time (in days) for which people
+--    claim they'll need a book and the average time (in days) for which a book
+--    is actually checked out.
+
+
+-- 5. Get a table which shows how many books have been checked out on each day
+--    since April 1 2022. There should be one row for each day.
+
+
+-- 6. Get a table of the titles of all fiction books that have been checked
+--    out.
+
+
+-- 7. Get a table (including titles) of all books that have never been checked
+--    out.
+
+
+-- 8. Get a table containing the names of all people who checked out a book but
+--    have not yet returned it.
+
+
+-- 9. Get the title of the book that was checked out the most.
+
+
+-- 10. Get the full name of the person who checked out the most books.
